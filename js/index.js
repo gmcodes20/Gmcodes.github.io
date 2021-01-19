a66837ab8f12d65d27b373c148893506cb3a1188
Godwin Martins <gmocdes20@gmail.com>
2021-01-19 20:14:04 +0100

    changes to my work

--- js/index.js
+++ js/index.js
@@ -1,12 +1,25 @@
+window.onscroll = function () { myFunction() };
+
+var header = document.querySelector(".headed");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 
-navToggle.addEventListener('click',()=>{
+navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
 });
 
-navLinks.forEach(link =>{
+navLinks.forEach(link => {
   link.addEventListener('click', () => {
     document.body.classList.remove('nav-open');
   })
-}) 
+})
+
